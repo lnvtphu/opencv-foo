daa3895267515b2f738426bebe1956b699a4826a
Phu Le <lnvtphu@gmail.com>
2017-11-11 10:44:10 +0700

    Update

--- programs/foo/main.js
+++ programs/foo/main.js
@@ -15,7 +15,7 @@ var RED   = [0, 0, 255]; // B, G, R
 const camera = new cv.VideoCapture(2);
 // const camera2 = new cv.VideoCapture(2);
 
-const window = new cv.NamedWindow('Video Fuxk', 0);
+const window = new cv.NamedWindow('Camera 1', 0);
 // const window2 = new cv.NamedWindow('Video Fuxk 2', 2);
 
 var position;
@@ -37,54 +37,36 @@ setInterval( () => {
     for(i = 0; i < contours.size(); i++) {
       if(contours.area(i) > maxArea) {
         var moments = contours.moments(i);
-      position = contours.boundingRect(i);
+        position = contours.boundingRect(i);
 
-      var cgx = Math.round(moments.m10 / moments.m00);
-      var cgy = Math.round(moments.m01 / moments.m00);
-
-      var rotated_rect = contours.minAreaRect(i);
-      // var blob_angle_deg = rotated_rect.angle;
-      // if (rotated_rect.size.width < rotated_rect.size.height) {
-      //   blob_angle_deg = 90 + blob_angle_deg;
-      // }
-      // console.log(rotated_rect);
-      var x1 = rotated_rect.points[0];
-      var x2 = rotated_rect.points[1];
-      var x3 = rotated_rect.points[2];
-      var x4 = rotated_rect.points[3];
-      var angle = rotated_rect.angle;
-      angle = Math.abs(angle);
-      if (rotated_rect.size.width < rotated_rect.size.height) {
-        angle = 90 + angle;
-      }
-      // var angle = rotated_rect.angle;
-      // console.log(rotated_rect);
-      var pad = 1/(Math.pow(Math.cos(angle), 2) - Math.pow(Math.sin(angle), 2));
-      var pad1 =  position.width*Math.cos(angle) - position.height*Math.sin(angle);
-      var pad2 =  -position.width*Math.sin(angle) + position.height*Math.cos(angle);
-      var d1 = pad * pad1;
-      var d2 = pad * pad2;
-      // console.log('D1: ' + d1);
-      // console.log('D2: ' + d2);
-      // console.log(angle);
-      
-      if( position.width <  widthOb){
-        widthOb = position.width;
-      }
-      sizePixel = size/widthOb;
-
-        // im.rectangle([position.x, position.y],[position.width,position.height], RED, 2);
-        // im.putText('x1', 'label', (x1.x, x1.y), 0.2,(255,255,255),2,2);
-        // im.putText('x2', 'label', (x2.x, x2.y), 0.2,(255,255,255),2,2);
-        // im.putText('x3', 'label', (x3.x, x3.y), 0.2,(255,255,255),2,2);
-        // im.putText('x4', 'label', (x4.x, x4.y), 0.2,(255,255,255),2,2);
-
+        var rotated_rect = contours.minAreaRect(i);
+        // 4 dinh cua hcn 
+        var x1 = rotated_rect.points[0];
+        var x2 = rotated_rect.points[1];
+        var x3 = rotated_rect.points[2];
+        var x4 = rotated_rect.points[3];
+        var angle = rotated_rect.angle;
+        
+        if( position.width <  widthOb){
+          widthOb = position.width;
+        }
+        sizePixel = size/widthOb;
+        // ve khung hcn 
+        im.rectangle([position.x, position.y],[position.width,position.height], RED, 2);
+          // im.putText('x1', 'label', (x1.x, x1.y), 0.2,(255,255,255),2,2);
+          // im.putText('x2', 'label', (x2.x, x2.y), 0.2,(255,255,255),2,2);
+          // im.putText('x3', 'label', (x3.x, x3.y), 0.2,(255,255,255),2,2);
+          // im.putText('x4', 'label', (x4.x, x4.y), 0.2,(255,255,255),2,2);
+        // ve khung hcn theo 4 dinh 
         im.line([x1.x,x1.y], [x2.x, x2.y], (255,0,0), 2);
         im.line([x2.x,x2.y], [x3.x, x3.y], (255,0,0), 2);
         im.line([x3.x,x3.y], [x4.x, x4.y], (255,0,0), 2);
         im.line([x4.x,x4.y], [x1.x, x1.y], (255,0,0), 2);
-        
-        im.putText(position.width+'-'+position.height, 'label', (70, 70), 0.2,(255,255,255),1,1);
+        // hien thong so
+
+        im.putText('HCN dung: '+position.width+'-'+position.height, 'label', (70, 70), 0.2,(255,255,255),1,1);
+        im.putText('HCN theo vat: '+Math.round(rotated_rect.size.width)+'-'+Math.round(rotated_rect.size.height), 'label', (120, 120), 0.2,(255,255,255),1,1);
+        im.putText('Goc: '+Math.round(angle), 'label', (170, 170), 0.2,(255,255,255),1,1);
         
       }
     }
